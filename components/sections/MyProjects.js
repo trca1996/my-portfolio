eefdca94d51252f4164ea530fb0a2cb83dfa0d28
trca1996 <igor.trnko@yahoo.com>
2021-08-02 00:12:33 +0200

    MyProject section started

---
+++ components/sections/MyProjects.js
@@ -0,0 +1,20 @@
+import SectionLayout from '../layout/SectionLayout'
+
+const MyProjects = () => {
+  return (
+    <SectionLayout title="Some Things I’ve Build">
+      <div>
+        <h1>Hellooo</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora ad
+          excepturi esse sapiente perferendis culpa saepe odit autem possimus,
+          ducimus rerum? Aperiam autem vel repellat reprehenderit quo nisi modi?
+          Et.
+        </p>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+    </SectionLayout>
+  )
+}
+
+export default MyProjects
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import Hero from '../components/sections/Hero'
 import { Fragment } from 'react'
 import AboutMe from '../components/sections/AboutMe'
 import SocialNetworks from '../components/SocialNetworks'
+import MyProjects from '../components/sections/MyProjects'
 
 const Home = () => {
   return (
     <Fragment>
       <Hero />
       <AboutMe />
+      <MyProjects />
       <SocialNetworks />
     </Fragment>
   )
