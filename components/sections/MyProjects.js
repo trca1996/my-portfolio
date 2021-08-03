ab37b1db0eab3ccc441cec902779b8e116b81678
trca1996 <igor.trnko@yahoo.com>
2021-08-03 14:11:59 +0200

    delete console.log

--- components/sections/MyProjects.js
+++ components/sections/MyProjects.js
@@ -2,7 +2,6 @@ import ProjectCard from '../cards/ProjectCard'
 import SectionLayout from '../layout/SectionLayout'
 
 const MyProjects = ({ projects }) => {
-  console.log(projects)
   return (
     <SectionLayout title="Some Things I’ve Build">
       {projects.map(
