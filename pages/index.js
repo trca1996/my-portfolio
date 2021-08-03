d96ba57111c5a45e5c2e47703b038d1200ce3234
trca1996 <igor.trnko@yahoo.com>
2021-08-03 15:08:17 +0200

    fix minor problem

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Fragment } from 'react'
 import AboutMe from '../components/sections/AboutMe'
 import SocialNetworks from '../components/SocialNetworks'
 import MyProjects from '../components/sections/MyProjects'
+import { connectToDatabase } from '../helper/db-util'
 
 const Home = (props) => {
   const projects = props.data
@@ -18,13 +19,16 @@ const Home = (props) => {
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch('http://localhost:3000/api/projects')
-  const data = await res.json()
+export async function getServerSideProps() {
+  const { db } = await connectToDatabase()
+  const data = await db
+    .collection('projects')
+    .find({})
+    .sort({ _id: -1 })
+    .toArray()
 
   return {
-    props: { data },
-    revalidate: 300,
+    props: { data: JSON.parse(JSON.stringify(data)) },
   }
 }
 
