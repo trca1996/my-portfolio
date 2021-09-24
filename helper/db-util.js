34dcd2e2f9292588c747b77ec32d9791ddbc18d0
trca1996 <igor.trnko@yahoo.com>
2021-09-24 19:46:43 +0200

    implement add and delete project routes

--- helper/db-util.js
+++ helper/db-util.js
@@ -1,18 +1,18 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from "mongodb";
 
-const MONGODB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ms6dw.mongodb.net/projects?retryWrites=true&w=majority`
-const MONGODB_DB = 'projects'
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ms6dw.mongodb.net/projects?retryWrites=true&w=majority`;
+const MONGODB_DB = "projects";
 
 if (!MONGODB_URI) {
   throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local',
-  )
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
 }
 
 if (!MONGODB_DB) {
   throw new Error(
-    'Please define the MONGODB_DB environment variable inside .env.local',
-  )
+    "Please define the MONGODB_DB environment variable inside .env.local"
+  );
 }
 
 /**
@@ -20,42 +20,43 @@ if (!MONGODB_DB) {
  * in development. This prevents connections growing exponentially
  * during API Route usage.
  */
-let cached = global.mongo
+let cached = global.mongo;
 
 if (!cached) {
-  cached = global.mongo = { conn: null, promise: null }
+  cached = global.mongo = { conn: null, promise: null };
 }
 
 export async function connectToDatabase() {
   if (cached.conn) {
-    return cached.conn
+    return cached.conn;
   }
 
   if (!cached.promise) {
     const opts = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    }
+    };
 
     cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
       return {
         client,
         db: client.db(MONGODB_DB),
-      }
-    })
+      };
+    });
   }
-  cached.conn = await cached.promise
-  return cached.conn
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
+// GET ALL PROJECTS
 export async function getAllProjects(collection) {
-  const { db } = await connectToDatabase()
+  const { db } = await connectToDatabase();
 
   const documents = await db
     .collection(collection)
     .find()
     .sort({ _id: -1 })
-    .toArray()
+    .toArray();
 
-  return documents
+  return documents;
 }
--- pages/api/projects.js
+++ pages/api/projects.js
@@ -1,11 +1,112 @@
-import { getAllProjects } from '../../helper/db-util'
+import nextConnect from "next-connect";
+import multer from "multer";
+import sharp from "sharp";
+import fs from "fs/promises";
+import slugify from "slugify";
+import { connectToDatabase } from "../../helper/db-util";
 
-const projects = async (req, res, next) => {
-  if (req.method === 'GET') {
-    const projects = await getAllProjects('projects')
+// SLUG NAMING
+const slug = (name) => {
+  return slugify(name, {
+    replacement: "_",
+    lower: true,
+  });
+};
 
-    return res.status(200).json(projects)
+// MULTER MIDDLEWARE
+const multerStorage = multer.memoryStorage();
+const multerFilter = (req, file, cb) => {
+  if (req.body.admin !== process.env.ADMIN) {
+    return cb(new Error("You are not authorized!"), false);
+  } else {
+    cb(null, true);
   }
-}
+};
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+const uploadMiddleware = upload.single("image");
 
-export default projects
+// SHARP RESIZE IMAGE MIDDLEWARE
+const resizeImage = async (req, res, next) => {
+  if (!req.file) return next();
+
+  const imageSlug = slug(req.body.name);
+
+  req.body.imageName = `${imageSlug}.jpeg`;
+  await sharp(req.file.buffer)
+    .resize(2000, 1333)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(
+      process.cwd() + `\\public\\images\\projects\\${req.body.imageName}`
+    );
+
+  next();
+};
+
+const projects = nextConnect({
+  onError(error, req, res) {
+    res
+      .status(501)
+      .json({ message: `Sorry something Happened! ${error.message}` });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ message: `Method '${req.method}' Not Allowed` });
+  },
+});
+
+// ADD PROJECT
+projects
+  .use(uploadMiddleware)
+  .use(resizeImage)
+  .post(async (req, res) => {
+    const { name, description, techStr, siteURL, gitHub } = req.body;
+    const tech = techStr.split(",");
+    const image = `/images/projects/${req.body.imageName}`;
+    const newProject = {
+      name,
+      description,
+      tech,
+      siteURL,
+      gitHub,
+      image,
+    };
+
+    const { db } = await connectToDatabase();
+
+    await db.collection("projects").insertOne(newProject);
+
+    res.status(200).json({
+      newProject,
+    });
+  })
+  // DELETE PROJECT
+  .delete(async (req, res) => {
+    const { admin, name } = req.body;
+
+    if (admin !== process.env.ADMIN) {
+      return res.status(401).json({ message: "You are not authorized!!!" });
+    }
+
+    const imageSlug = slug(name);
+
+    const imagePath =
+      process.cwd() + `\\public\\images\\projects\\${imageSlug}.jpeg`;
+
+    const { db } = await connectToDatabase();
+    await db.collection("projects").deleteOne({ name: name });
+
+    await fs.unlink(imagePath);
+
+    res.status(200).json({ message: "Project is deleted!" });
+  });
+
+export default projects;
+
+export const config = {
+  api: {
+    bodyParser: false, // Disallow body parsing, consume as stream
+  },
+};
